5219a46b7c13c43a1c9f198f275d36bb41c867bc
Yining Wang <wangyining203@gmail.com>
2021-11-06 23:15:58 +0800

    add some methods to GUIUtil.java

--- src/utils/GUIUtil.java
+++ src/utils/GUIUtil.java
@@ -1,9 +1,11 @@
 package utils;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 public class GUIUtil {
-
+    private static String imageFolder = "./img";
     //check the blanket is empty or not
     public static boolean checkEmpty(JTextField textField,String input){
         String text = textField.getText().trim();
@@ -18,7 +20,7 @@ public class GUIUtil {
 
     //check the blanket is number or not
     public static boolean checkNumber(JTextField textField, String input){
-        if (checkEmpty(textField, input)){
+        if (!checkEmpty(textField, input)){
             return false;
         }
         String text = textField.getText().trim();
@@ -29,6 +31,45 @@ public class GUIUtil {
             JOptionPane.showMessageDialog(null, input + " 需要是整数");
             textField.grabFocus();
             return false;
+        }
+    }
+
+    //check the blanket is zero or not
+    public static boolean checkZero(JTextField textField, String input){
+        if (!checkEmpty(textField, input)){
+            return false;
+        }
+        String text = textField.getText().trim();
+        if (Integer.parseInt(text) == 0){
+            textField.grabFocus();
+            return false;
+        }
+        return true;
+    }
+
+    //set the colors
+    public static void setColor(Color color, JComponent... components) {
+        for (JComponent c : components) {
+            c.setForeground(color);
+        }
+    }
+
+    public static void setImageIcon(JButton b, String fileName, String tip) {
+        ImageIcon i = new ImageIcon(new File(imageFolder, fileName).getAbsolutePath());
+        b.setIcon(i);
+        b.setPreferredSize(new Dimension(61, 81));
+        b.setToolTipText(tip);
+        b.setVerticalTextPosition(JButton.BOTTOM);
+        b.setHorizontalTextPosition(JButton.CENTER);
+        b.setText(tip);
+    }
+
+    public static void useLNF() {
+        try {
+            javax.swing.UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
