654d6e46aa72674753b4cb791669b19b5df3a7b9
Yining Wang <wangyining203@gmail.com>
2021-11-08 22:30:24 +0800

    add the SpendPanel

--- src/gui/panel/SpendPanel.java
+++ src/gui/panel/SpendPanel.java
@@ -1,35 +1,92 @@
 package gui.panel;
 
+import utils.CircleProgressBar;
+import utils.ColorUtil;
+import utils.GUIUtil;
+
 import javax.swing.*;
+import java.awt.*;
 
-public class SpendPanel {
-    //私有化构造方法使得该类无法在外部通过new 进行实例化
-    private SpendPanel(){
+public class SpendPanel extends JPanel{
+    static{
+        GUIUtil.useLNF();
     }
-    public static SpendPanel instance;
 
-    //the labels of spend panel
-    JLabel lMonthSpend = new JLabel("本月消费");
-    JLabel lTodaySpend = new JLabel("今日消费");
-    JLabel lAvgSpendPerDay = new JLabel("日均消费");
-    JLabel lMonthLeft = new JLabel("本月剩余");
-    JLabel lDayAvgAvailable = new JLabel("日均可用");
-    JLabel lMonthLeftDay = new JLabel("距离月末");
+    public static SpendPanel instance = new SpendPanel();
+    public JLabel lMonthSpend = new JLabel("本月消费");
+    public JLabel lTodaySpend = new JLabel("今日消费");
+    public JLabel lAvgSpendPerDay = new JLabel("日均消费");
+    public JLabel lMonthLeft = new JLabel("本月剩余");
+    public JLabel lDayAvgAvailable = new JLabel("日均可用");
+    public JLabel lMonthLeftDay = new JLabel("距离月末");
 
-    JLabel vMonthSpend = new JLabel("￥2300");
-    JLabel vTodaySpend = new JLabel("￥25");
-    JLabel vAvgSpendPerDay = new JLabel("￥120");
-    JLabel vMonthAvailable = new JLabel("￥2084");
-    JLabel vDayAvgAvailable = new JLabel("￥389");
-    JLabel vMonthLeftDay = new JLabel("15天");
+    public JLabel vMonthSpend = new JLabel("￥2300");
+    public JLabel vTodaySpend = new JLabel("￥25");
+    public JLabel vAvgSpendPerDay = new JLabel("￥120");
+    public JLabel vMonthAvailable = new JLabel("￥2084");
+    public JLabel vDayAvgAvailable = new JLabel("￥389");
+    public JLabel vMonthLeftDay = new JLabel("15天");
 
-    //public static 方法，返回实例对象
-    public static SpendPanel getInstance(){
-        //第一次访问的时候，发现instance没有指向任何对象，这时实例化一个对象
-        if(null==instance){
-            instance = new SpendPanel();
-        }
-        //返回 instance指向的对象
-        return instance;
+    CircleProgressBar bar;
+
+    public SpendPanel() {
+        this.setLayout(new BorderLayout());
+        bar = new CircleProgressBar();
+        bar.setBackgroundColor(ColorUtil.blueColor);
+
+        GUIUtil.setColor(ColorUtil.grayColor, lMonthSpend, lTodaySpend, lAvgSpendPerDay, lMonthLeft, lDayAvgAvailable,
+                lMonthLeftDay, vAvgSpendPerDay, vMonthAvailable, vDayAvgAvailable, vMonthLeftDay);
+        GUIUtil.setColor(ColorUtil.blueColor, vMonthSpend, vTodaySpend);
+
+        vMonthSpend.setFont(new Font("微软雅黑", Font.BOLD, 23));
+        vTodaySpend.setFont(new Font("微软雅黑", Font.BOLD, 23));
+
+        this.add(center(), BorderLayout.CENTER);
+        this.add(south(), BorderLayout.SOUTH);
+
     }
+
+    private JPanel center() {
+        JPanel p = new JPanel();
+        p.setLayout(new BorderLayout());
+        p.add(west(), BorderLayout.WEST);
+        p.add(center2(),BorderLayout.CENTER);
+
+        return p;
+    }
+
+    private Component center2() {
+        return bar;
+    }
+
+    private Component west() {
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(4, 1));
+        p.add(lMonthSpend);
+        p.add(vMonthSpend);
+        p.add(lTodaySpend);
+        p.add(vTodaySpend);
+        return p;
+    }
+
+    private JPanel south() {
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(2, 4));
+
+        p.add(lAvgSpendPerDay);
+        p.add(lMonthLeft);
+        p.add(lDayAvgAvailable);
+        p.add(lMonthLeftDay);
+        p.add(vAvgSpendPerDay);
+        p.add(vMonthAvailable);
+        p.add(vDayAvgAvailable);
+        p.add(vMonthLeftDay);
+
+        return p;
+    }
+
+    public static void main(String[] args) {
+        GUIUtil.showPanel(SpendPanel.instance);
+    }
+
 }
