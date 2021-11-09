c12d75fc0130f08ed95d53adef405ba0746172b8
Yining Wang <wangyining203@gmail.com>
2021-11-09 21:31:06 +0800

    add some panels

---
+++ src/gui/model/CategoryTableModel.java
@@ -0,0 +1,71 @@
+package gui.model;
+
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryTableModel implements TableModel {
+
+    String[] columnNames = new String[]{"分类名称","消费次数"};
+    List<String> cs = new ArrayList<>();
+
+    public CategoryTableModel(){
+        cs.add("餐饮");
+        cs.add("交通");
+        cs.add("住宿");
+        cs.add("话费");
+    }
+
+    @Override
+    public int getRowCount() {
+        return cs.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public String getColumnName(int index) {
+        return columnNames[index];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int i) {
+        //反射，String.class可以返回一个代表String类的类对象
+        return String.class;
+    }
+
+    @Override
+    public boolean isCellEditable(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        if(columnIndex == 0) {
+            return cs.get(rowIndex);
+        }
+        if(columnIndex == 1) {
+            return 0;
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object o, int i, int i1) {
+
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener tableModelListener) {
+
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener tableModelListener) {
+
+    }
+}
---
+++ src/gui/panel/RecordPanel.java
@@ -0,0 +1,62 @@
+package gui.panel;
+
+import gui.model.CategoryComboBoxModel;
+import org.jdesktop.swingx.JXDatePicker;
+import utils.ColorUtil;
+import utils.GUIUtil;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
+public class RecordPanel extends JPanel {
+    static {
+        GUIUtil.useLNF();
+    }
+
+    public static RecordPanel instance = new RecordPanel();
+
+    JLabel lSpend = new JLabel("花费(￥)");
+    JLabel lCategory = new JLabel("分类");
+    JLabel lComment = new JLabel("备注");
+    JLabel lDate = new JLabel("日期");
+
+    public JTextField tfSpend = new JTextField("0");
+
+    public CategoryComboBoxModel cbModel = new CategoryComboBoxModel();
+    public JComboBox<String> cbCategory = new JComboBox<>(cbModel);
+    public JTextField tfComment = new JTextField();
+    public JXDatePicker datepick = new JXDatePicker(new Date());
+
+    JButton bSubmit = new JButton("记一笔");
+
+    public RecordPanel() {
+        GUIUtil.setColor(ColorUtil.grayColor, lSpend,lCategory,lComment,lDate);
+        GUIUtil.setColor(ColorUtil.blueColor, bSubmit);
+        JPanel pInput =new JPanel();
+        JPanel pSubmit = new JPanel();
+        int gap = 40;
+        pInput.setLayout(new GridLayout(4,2,gap,gap));
+
+        pInput.add(lSpend);
+        pInput.add(tfSpend);
+        pInput.add(lCategory);
+        pInput.add(cbCategory);
+        pInput.add(lComment);
+        pInput.add(tfComment);
+        pInput.add(lDate);
+        pInput.add(datepick);
+
+        pSubmit.add(bSubmit);
+
+        this.setLayout(new BorderLayout());
+        this.add(pInput,BorderLayout.NORTH);
+        this.add(pSubmit,BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        GUIUtil.showPanel(RecordPanel.instance);
+    }
+
+
+}
