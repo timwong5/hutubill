2530e4ddb49099174369cefc77673acc2dcf20ec
Yining Wang <wangyining203@gmail.com>
2021-11-10 00:06:08 +0800

    add some entities

---
+++ src/entity/Config.java
@@ -0,0 +1,36 @@
+package entity;
+
+/**
+ * id Ö÷¼ü
+ * key ¼ü
+ * value Öµ
+ */
+public class Config {
+    public int id;
+    public String key;
+    public String value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/entity/Record.java
@@ -0,0 +1,58 @@
+package entity;
+
+import java.util.Date;
+
+/**
+ * id 主键
+ * spend 消费金额
+ * cid 分类信息id
+ * comment 备注
+ * date 日期
+ */
+public class Record {
+    public int spend;
+    public int id;
+    public int cid;
+    public String comment;
+    public Date date;
+
+    public int getSpend() {
+        return spend;
+    }
+
+    public void setSpend(int spend) {
+        this.spend = spend;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
