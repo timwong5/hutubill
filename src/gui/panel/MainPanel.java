abd346e4c2efc6955a311fb92ede6a76b8161bba
Yining Wang <wangyining203@gmail.com>
2021-11-12 19:40:57 +0800

    add ToolBarListener

--- src/gui/panel/MainPanel.java
+++ src/gui/panel/MainPanel.java
@@ -1,5 +1,6 @@
 package gui.panel;
 
+import gui.listener.ToolBarListener;
 import utils.CenterPanel;
 import utils.GUIUtil;
 
@@ -45,6 +46,19 @@ public class MainPanel extends JPanel {
         setLayout(new BorderLayout());
         add(tb, BorderLayout.NORTH);
         add(workingPanel, BorderLayout.CENTER);
+        addListener();
+
+    }
+
+    private void addListener(){
+        ToolBarListener listener = new ToolBarListener();
+        bSpend.addActionListener(listener);
+        bRecord.addActionListener(listener);
+        bCategory.addActionListener(listener);
+        bReport.addActionListener(listener);
+        bConfig.addActionListener(listener);
+        bBackup.addActionListener(listener);
+        bRecover.addActionListener(listener);
     }
 
     public static void main(String[] args) {
---
+++ src/startup/Bootstrap.java
@@ -0,0 +1,24 @@
+package startup;
+
+
+import gui.frame.MainFrame;
+import gui.panel.MainPanel;
+import gui.panel.SpendPanel;
+import utils.GUIUtil;
+
+import javax.swing.*;
+import java.lang.reflect.InvocationTargetException;
+
+public class Bootstrap {
+    public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+        GUIUtil.useLNF();
+
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame.instance.setVisible(true);
+                MainPanel.instance.workingPanel.show(SpendPanel.instance);
+            }
+        });
+    }
+}
